33f035f93551e153639c76d7ea94d57f7ab01c3a
Kyle Lyttle <klyttle09@qub.ac.uk>
2024-03-05 19:37:15 +0000

    SQL Create statement fixed for student, including auto increment

--- SQL/Student/createStudent.sql
+++ SQL/Student/createStudent.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Student (
-    id BIGINT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     student_number VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
